f562f86be85393826c05b6be15f356c7259eb6ce
GhadeerZahwe <GhadeerZahwe>
2024-02-03 21:56:15 +0200

    Increase the maximum execution time limit

--- Pull_Request/app/Http/Controllers/WooCommerceController.php
+++ Pull_Request/app/Http/Controllers/WooCommerceController.php
@@ -8,33 +8,35 @@ class WooCommerceController extends Controller
 {
     public function getPullRequests()
     {
+        // Increase the maximum execution time limit
+        set_time_limit(40);
 
+        for ($i = 1; $i <= 3; $i++) {
+            $url = "https://api.github.com/repos/woocommerce/woocommerce/pulls?per_page=100&page=" . $i;
+            $headers = [
+                "Accept:application/vnd.github+json", 
+                "User-Agent: GhadeerZahwe"
+            ];
+            $curl = curl_init($url);
 
-        $url = "https://api.github.com/repos/woocommerce/woocommerce/pulls?per_page=100";
-        $headers = [
-         "Accept:application/vnd.github+json",
-         "User-Agent: GhadeerZahwe"
-        ];
-        $curl = curl_init($url);
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+            $resp = curl_exec($curl);
+            curl_close($curl);
 
+            $pull_requests = json_decode($resp, false);
 
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        $resp = curl_exec($curl);
-
-        curl_close($curl);
-
-        $pull_requests = json_decode($resp, false);
-        for($i=0;$i<count($pull_requests);$i++){
-            print_r($pull_requests[$i]->url . "\n");
+            if (is_array($pull_requests)) {
+                foreach ($pull_requests as $pull_request) {
+                    echo $pull_request->url . "\n";
+                }
+                echo "Number of pull requests: " . count($pull_requests) . "\n";
+            } else {
+                echo "Error decoding JSON response.\n";
+            }
         }
-        print_r(count($pull_requests));
-
-        $mydate = date("Y-m-d", strtotime("-2 week"));
-        print_r($mydate);
     }
 }
