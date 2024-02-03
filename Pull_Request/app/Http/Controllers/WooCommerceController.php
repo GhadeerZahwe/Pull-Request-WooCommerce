d302c6b49aa0ca2308ce8dd864f05c36ba89876e
GhadeerZahwe <GhadeerZahwe>
2024-02-03 21:41:28 +0200

    creating a for loop to get all the the requests due to limitations

--- Pull_Request/app/Http/Controllers/WooCommerceController.php
+++ Pull_Request/app/Http/Controllers/WooCommerceController.php
@@ -8,16 +8,19 @@ class WooCommerceController extends Controller
 {
     public function getPullRequests()
     {
-        $url = "https://api.github.com/repos/woocommerce/woocommerce/pulls";
-        $headers = ["Accept:application/vnd.github+json",
-        "User-Agent: GhadeerZahwe"];
+
+
+        $url = "https://api.github.com/repos/woocommerce/woocommerce/pulls?per_page=100";
+        $headers = [
+         "Accept:application/vnd.github+json",
+         "User-Agent: GhadeerZahwe"
+        ];
         $curl = curl_init($url);
 
 
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-        //for debug only!
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
@@ -26,13 +29,12 @@ class WooCommerceController extends Controller
         curl_close($curl);
 
         $pull_requests = json_decode($resp, false);
+        for($i=0;$i<count($pull_requests);$i++){
+            print_r($pull_requests[$i]->url . "\n");
+        }
+        print_r(count($pull_requests));
 
-       // for($i=0;$i<count($pull_requests);$i++){
-        //     print_r($pull_requests[$i]->url . "\n");
-        // }
-
-        $mydate=date("Y-m-d", strtotime("-2 week"));
-        print_r($mydate); 
-
+        $mydate = date("Y-m-d", strtotime("-2 week"));
+        print_r($mydate);
     }
 }
